fbc09e07ae9b5fcd99d7880939e59c4fd166e710
TRM-coding <22301131@bjtu.edu.cn>
2024-09-25 23:28:01 +0800

    完成tensor.cpp

--- build/3dparty/armadillo/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Science_research/Eckart-Young-based-ML-Inference-framework/3dparty/armadillo
-# Build directory: /home/Science_research/Eckart-Young-based-ML-Inference-framework/build/3dparty/armadillo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests1")
--- build/3dparty/googleTest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Science_research/Eckart-Young-based-ML-Inference-framework/3dparty/googleTest/googlemock
-# Build directory: /home/Science_research/Eckart-Young-based-ML-Inference-framework/build/3dparty/googleTest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
