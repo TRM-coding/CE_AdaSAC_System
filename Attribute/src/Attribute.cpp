c15c11b95e32f933bf3b43693d89f233640594df
TRM-coding <22301131@bjtu.edu.cn>
2024-10-09 22:09:34 +0800

    更新attribute

--- Attribute/src/Attribute.cpp
+++ Attribute/src/Attribute.cpp
@@ -1,2 +1,19 @@
 #include <Attribute.hpp>
 
+namespace MINI_MLsys {
+std::vector<float> Attribute::get(bool clear_origin_data) {
+  assert(!data.empty());
+  std::vector<float> res;
+  const uint32_t float_size = sizeof(float);
+  assert(data.size() % float_size == 0);
+  for (uint32_t i = 0; i < data.size(); i += float_size) {
+    float value;
+    memcpy(&value, &data[i], float_size);
+    res.push_back(value);
+  }
+  if (clear_origin_data) {
+    data.clear();
+  }
+  return res;
+}
+} // namespace MINI_MLsys
--- include/Attribute.hpp
+++ include/Attribute.hpp
@@ -11,24 +11,8 @@ namespace MINI_MLsys
   {
     std::vector<char> data;
     std::vector<int> shape;
-    std::vector<float> get(bool clear_origin_data)
-    {
-      assert(!data.empty());
-      std::vector<float> res;
-      const uint32_t float_size = sizeof(float);
-      assert(data.size() % float_size == 0);
-      for (uint32_t i = 0; i < data.size(); i += float_size)
-      {
-        float value;
-        memcpy(&value, &data[i], float_size);
-        res.push_back(value);
-      }
-      if (clear_origin_data)
-      {
-        data.clear();
-      }
-      return res;
-    }
+    std::vector<float> get(bool clear_origin_data);
+    Attribute(std::vector<float> &data, std::vector<int> &shape) : data(data.begin(), data.end()), shape(shape.begin(), shape.end()){};
   };
 }
 #endif
